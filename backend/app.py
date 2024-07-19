f4a63a9b24246062db479060f28f47678fad4442
berrli <liam.j.berrisford@bath.edu>
2024-07-19 08:37:46 +0100

    update .yml file

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,6 @@ import os
 from flask_cors import CORS
 import geopandas as gpd
 import pandas as pd
-import sqlite3
 
 app = Flask(__name__, static_folder='build', static_url_path='')
 CORS(app)
@@ -23,36 +22,42 @@ def geojson_data():
     # Read the GeoPackage file for the grids layer
     grids = gpd.read_file('data/database.gpkg', layer="grids")
 
-    # Create a connection to the GeoPackage using sqlite3
-    conn = sqlite3.connect('data/database.gpkg')
-
-    # Define the columns to read based on the selected data type
-    if data_type == 'nox':
-        columns = ['"Grid ID"', '"nox prediction mean"']
-    elif data_type == 'no2':
-        columns = ['"Grid ID"', '"no2 prediction mean"']
-    else:
-        return jsonify({"error": "Invalid data type"}), 400
-
-    # Create the SQL query to select only the required columns
-    sql_query = 'SELECT * FROM air_pollution_concentrations'
-
-
-    # Read the required columns using pandas
-    air_pollution_concentrations = pd.read_sql_query(sql_query, conn)
-
-    # Close the connection
-    conn.close()
-
-    # Merge or join the grids with the selected columns of air_pollution_concentrations
-    # Assuming a common column 'Grid ID' exists in both GeoDataFrames
-    merged_data = grids.merge(air_pollution_concentrations, left_on='Grid ID', right_on='"Grid ID"')
-
     # Reproject to EPSG:4326 (WGS 84)
-    merged_data = merged_data.to_crs(epsg=4326)
+    grids = grids.to_crs(epsg=4326)
 
     # Convert the GeoDataFrame to GeoJSON
-    geojson_data = merged_data.to_json()
+    geojson_data = grids.to_json()
+
+    # # Create a connection to the GeoPackage using sqlite3
+    # conn = sqlite3.connect('data/database.gpkg')
+
+    # # Define the columns to read based on the selected data type
+    # if data_type == 'nox':
+    #     columns = ['"Grid ID"', '"nox prediction mean"']
+    # elif data_type == 'no2':
+    #     columns = ['"Grid ID"', '"no2 prediction mean"']
+    # else:
+    #     return jsonify({"error": "Invalid data type"}), 400
+
+    # # Create the SQL query to select only the required columns
+    # sql_query = 'SELECT * FROM air_pollution_concentrations'
+
+
+    # # Read the required columns using pandas
+    # air_pollution_concentrations = pd.read_sql_query(sql_query, conn)
+
+    # # Close the connection
+    # conn.close()
+
+    # # Merge or join the grids with the selected columns of air_pollution_concentrations
+    # # Assuming a common column 'Grid ID' exists in both GeoDataFrames
+    # merged_data = grids.merge(air_pollution_concentrations, left_on='Grid ID', right_on='"Grid ID"')
+
+    # # Reproject to EPSG:4326 (WGS 84)
+    # merged_data = merged_data.to_crs(epsg=4326)
+
+    # # Convert the GeoDataFrame to GeoJSON
+    # geojson_data = merged_data.to_json()
 
     return geojson_data
 @app.route('/predict', methods=['POST'])
